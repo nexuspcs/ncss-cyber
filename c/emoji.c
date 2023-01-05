0a41c4b1077e34367225d45d1cdf444dc63d0921
James Coates <69493073+nexuspcs@users.noreply.github.com>
2023-01-05 17:23:53 +1100

    Add files via upload

---
+++ c/emoji.c
@@ -0,0 +1,21 @@
+// this program will simply display an emoji
+// -- james coates 5/1/23 (last updated)
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int arc, char** argv) {
+    printf("                                                 \n");
+    printf("      ______                    ______           \n");
+    printf("     /                        /                  \n");
+    printf("    |    .   |                |    .   |         \n");
+    printf("       ______/                   ______/         \n");
+    printf("                      x                          \n");
+    printf("        |                          |             \n");
+    printf("         |                        |              \n");
+    printf("          |______________________|               \n");
+
+    printf("\n\n\n");
+    return EXIT_SUCCESS;
+}
+
